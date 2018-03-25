cad059e6d658553fc1139b3dd8b64bcb1b12aee5
ZCL <dickmrbean@gmail.com>
2018-03-25 21:37:13 +0800

    Mold

--- HoloReader.py
+++ HoloReader.py
@@ -12,8 +12,15 @@ def upload():
     img_base64 = img['image']
     img_file = decode.load_img_bytes(img_base64)
     word = decode.closest_word(img_file)
-    definition = word_to_def.google_translate_en_to_zh(word)
-    return definition
+    def_entocn = word_to_def.google_translate_en_to_zh(word)
+    def_entoen_raw = word_to_def.oxford_en_to_en(word)
+    if len(def_entoen_raw) == 1:
+        def_entoen = def_entoen_raw[0]["entries"][0]["senses"][0]["definitions"][0]
+    else:
+        def_entoen = ""
+        for j, i in enumerate(def_entoen_raw):
+            def_entoen += "{}.".format(j + 1), i["entries"][0]["senses"][0]["definitions"][0]
+    return def_entoen
 
 
 if __name__ == '__main__':
